dc30fbb93b5d89c3a27d7349e1c5039b9b94dfdb
ctweehaur <ctweehaur@gmail.com>
2026-06-02 23:22:53 +0800

    Update lanyuchongshu_translation.js

--- lanyuchongshu_translation.js
+++ lanyuchongshu_translation.js
@@ -1,7 +1,7 @@
 const translationData = [
     {
         zh: "齐宣王派人吹竽，每次都必须有三百人一起合奏。南郭先生请求给齐宣王吹竽，齐宣王很高兴，发给他的粮食和待遇跟那几百个正规乐师一模一样。",
-        py: "qí xuān wáng pài rén chuī yú měi cì dōu bì xū yǒu sān bǎi rén yī qǐ hé zòu nán guō xiān shēng qǐng gěi qí xuān wáng chuī yú qí xuān wáng hěn gāo xìng fā gěi tā de liáng shí hé dài yù gēn nà jǐ bǎi gè zhèng guī yuè shī yī mú yī yàng",
+        py: "qí xuān wáng pài rén chuī yú měi cì dōu bì xū yǒu sān bǎi rén yī qǐ hé zòu nán guō xiān shēng qǐng qiú gěi qí xuān wáng chuī yú qí xuān wáng hěn gāo xìng fā gěi tā de liáng shí hé dài yù gēn nà jǐ bǎi gè zhèng guī yuè shī yī mú yī yàng",
         en: "King Xuan of Qi liked to listen to the yu, and he always wanted 300 people to play together. Mr. Nanguo asked to join the group. The king was happy and gave him the same high food and pay as the other real musicians."
     },
     {
