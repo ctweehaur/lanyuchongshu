d56f94655337455606ebfbdca3114aae5eccc9de
ctweehaur <ctweehaur@gmail.com>
2026-06-02 23:10:26 +0800

    Update lanyuchongshu_translation.js

--- lanyuchongshu_translation.js
+++ lanyuchongshu_translation.js
@@ -1,12 +1,12 @@
 const translationData = [
     {
         zh: "齐宣王派人吹竽，每次都必须有三百人一起合奏。南郭先生请求给齐宣王吹竽，齐宣王很高兴，发给他的粮食和待遇跟那几百个正规乐师一模一样。",
-        py: "qí xuān wáng shǐ rén chuī yú měi cì dōu bì xū yǒu sān bǎi rén yī qǐ hé zòu nán guō chǔ shì qǐng wèi wáng chuī yú xuān wáng yuè zhī fā gěi tā de lǐn sì gēn nà jǐ bǎi gè zhèng guī yuè shī yī mú yī yàng",
+        py: "qí xuān wáng pài rén chuī yú měi cì dōu bì xū yǒu sān bǎi rén yī qǐ hé zòu nán guō xiān shēng qǐng gěi qí xuān wáng chuī yú qí xuān wáng hěn gāo xìng fā gěi tā de liáng shí hé dài yù gēn nà jǐ bǎi gè zhèng guī yuè shī yī mú yī yàng",
         en: "King Xuan of Qi liked to listen to the yu, and he always wanted 300 people to play together. Mr. Nanguo asked to join the group. The king was happy and gave him the same high food and pay as the other real musicians."
     },
     {
         zh: "齐宣王死后，他的儿子齐湣王继承了王位。齐湣王喜欢听乐师们一个接一个地单独吹奏，南郭先生听到这个消息后，只好偷偷逃跑了。",
-        py: "qí xuān wáng sǐ hòu tā de ér zǐ qí mǐn wáng lì le wáng wèi qí mǐn wáng hào yī yī tīng zhī nán guō chǔ shì tīng dào zhè gè xiāo xī hòu zhǐ hǎo tōu tōu táo pǎo le",
+        py: "qí xuān wáng sǐ hòu tā de ér zǐ qí mǐn wáng jì chéng le wáng wèi qí mǐn wáng xǐ huān tīng yuè shī mén yī gè jiē yī gè dì dān dú chuī zòu nán guō xiān shēng tīng dào zhè gè xiāo xī hòu zhǐ hǎo tōu tōu táo pǎo le",
         en: "After King Xuan died, King Min became the new king. King Min liked to listen to the players one by one. When Mr. Nanguo heard this, he had to pack up and run away."
     }
 ];
